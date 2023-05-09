c6b0b23ad089858dd577a27e4b96ecef1074cbab
Juseailles <126775416+Js3ailles@users.noreply.github.com>
2023-05-10 00:25:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ import streamlit as st
 
 
 def split_audio(file_path, part_duration):
-    audio = AudioFileClip(file_path)
-    duration = audio.duration
-    parts = int(duration // part_duration)
+    try:
+        audio = AudioFileClip(file_path)
+        duration = audio.duration
+        parts = int(duration // part_duration)
 
-    temp_dir = TemporaryDirectory()
+        temp_dir = TemporaryDirectory()
 
-    for i in range(parts):
-        start_time = i * part_duration
-        end_time = (i + 1) * part_duration
-        split_audio = audio.subclip(start_time, end_time)
-        split_audio.write_audiofile(os.path.join(temp_dir.name, f"part_{i + 1}.mp3"), codec='mp3')
+        for i in range(parts):
+            start_time = i * part_duration
+            end_time = (i + 1) * part_duration
+            split_audio = audio.subclip(start_time, end_time)
+            split_audio.write_audiofile(os.path.join(temp_dir.name, f"part_{i + 1}.mp3"), codec='mp3')
 
-    return parts, temp_dir
+        return parts, temp_dir
+    except Exception as e:
+        st.error(f"Error in split_audio: {e}")
+        raise e
 
 
 def transcribe_audio_files(directory):
@@ -28,41 +32,49 @@ def transcribe_audio_files(directory):
         'x-gladia-key': st.secrets["GLADIA_KEY"],
     }
 
-    for filename in os.listdir(directory):
-        if filename.endswith(".mp3"):
-            file_path = os.path.join(directory, filename)
-            files = {
-                'audio': (filename, open(file_path, 'rb'), 'audio/mpeg'),
-                'diarization_max_speakers': (None, '2'),
-                'language': (None, 'french'),
-                'language_behaviour': (None, 'manual'),
-                'output_format': (None, 'txt'),
-                'target_translation_language': (None, 'english'),
-            }
+    try:
+        for filename in os.listdir(directory):
+            if filename.endswith(".mp3"):
+                file_path = os.path.join(directory, filename)
+                files = {
+                    'audio': (filename, open(file_path, 'rb'), 'audio/mpeg'),
+                    'diarization_max_speakers': (None, '2'),
+                    'language': (None, 'french'),
+                    'language_behaviour': (None, 'manual'),
+                    'output_format': (None, 'txt'),
+                    'target_translation_language': (None, 'english'),
+                }
 
-            response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
-            if response.status_code == 200:
-                responses.append(response)
-            else:
-                print(f"Request for {filename} failed with status code {response.status_code}: {response.text}")
+                response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
+                if response.status_code == 200:
+                    responses.append(response)
+                else:
+                    print(f"Request for {filename} failed with status code {response.status_code}: {response.text}")
 
-    return responses
+        return responses
+    except Exception as e:
+        st.error(f"Error in transcribe_audio_files: {e}")
+        raise e
 
 
 def transcribe_audio(input_file_path, segment_duration):
-    num_parts, temp_dir = split_audio(input_file_path, segment_duration)
-    resp = transcribe_audio_files(temp_dir.name)
-    predictions = []
-    for response in resp:
-        data = response.json()
-        if 'prediction' in data:
-            predictions.append(data['prediction'])
-        else:
-            print(f"Key 'prediction' not found in response from {response.url}")
+    try:
+        num_parts, temp_dir = split_audio(input_file_path, segment_duration)
+        resp = transcribe_audio_files(temp_dir.name)
+        predictions = []
+        for response in resp:
+            data = response.json()
+            if 'prediction' in data:
+                predictions.append(data['prediction'])
+            else:
+                print(f"Key 'prediction' not found in response from {response.url}")
 
-    transcript = "\n".join(predictions)
+        transcript = "\n".join(predictions)
 
-    return transcript
+        return transcript
+    except Exception as e:
+        st.error(f"Error in transcribe_audio: {e}")
+        raise e
 
 
 st.title("Audio Transcription App")
@@ -79,13 +91,16 @@ if uploaded_files:
 
     if st.button("Transcribe"):
         with st.spinner('Creating this audio transcript'):
-            with NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_audio_file:
-                tmp_audio_file.write(audio_files[selected_audio].getbuffer())
-                tmp_audio_file.flush()
-                st.write("en cours de chargement")
-                transcript = transcribe_audio(tmp_audio_file.name, 700)
-                st.write("voici le transcript")
-                st.session_state[f"transcript_{selected_audio}"] = transcript
+            try:
+                with NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_audio_file:
+                    tmp_audio_file.write(audio_files[selected_audio].getbuffer())
+                    tmp_audio_file.flush()
+                    st.write("en cours de chargement")
+                    transcript = transcribe_audio(tmp_audio_file.name, 700)
+                    st.write("voici le transcript")
+                    st.session_state[f"transcript_{selected_audio}"] = transcript
+            except Exception as e:
+                st.error(f"Error during transcription process: {e}")
 
     if st.session_state[f"transcript_{selected_audio}"] is not None:
         st.text_area("Transcription", value=st.session_state[f"transcript_{selected_audio}"], height=400)
