57f40832684e8f32c63c74db1ec28ee9ab6ea731
Juseailles <126775416+Js3ailles@users.noreply.github.com>
2023-05-22 10:53:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -248,7 +248,6 @@ elif choice == "PDF summarizer":
 
             if not paragraphs:
                 st.error("Unable to extract content from the PDF.")
-            return
 
             paragraphs = concatenate_short_paragraphs(paragraphs)
             # Reset the cache to avoid errors when re-running the code
